b98678d87263223de7171fbd75b7feb1f5418c98
Sydney Luong <sydney.luong@epitech.eu>
2025-12-10 17:01:38 +0100

    feat : adding OrbitControls, lil-GUI for early dev and Stats

--- main.js
+++ main.js
@@ -1,25 +1,52 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import GUI from 'three/addons/libs/lil-gui.module.min.js';
+import Stats from 'three/addons/libs/stats.module.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const gui = new GUI();
+
+const stats = new Stats();
+document.body.appendChild(stats.dom);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const MainLight = new THREE.DirectionalLight();
+MainLight.position.set(1, 2, 3);
+scene.add(MainLight);
+
+const AmbientLight = new THREE.AmbientLight();
+AmbientLight.intensity = 0.5;
+scene.add(AmbientLight);
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
+controls.update();
 
 function animate() {
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
+    controls.update();
+    stats.update();
     renderer.render(scene, camera);
 
 }
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+const cubeFolder = gui.addFolder('Cube');
+cubeFolder.add(cube.position, 'x', -2, 2, 0.01).name('Position X');
+cubeFolder.addColor(cube.material, 'color');
